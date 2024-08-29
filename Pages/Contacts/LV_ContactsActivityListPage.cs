f2760c2696b85f6f91f413e8a9c14cd5f67a195c
Sahil Mittal <sahil.mittal@technossus.com>
2024-08-29 16:54:13 +0530

    LV Activities Module scripting - TMTC0032668_VerifyMeetingsEventsActivitiesCreatedAsABankerInSF

---
+++ Pages/Contacts/LV_ContactsActivityListPage.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.DevTools.V120.DOM;
+using SF_Automation.TestData;
+using SF_Automation.UtilityFunctions;
+using System;
+using System.Threading;
+
+namespace SF_Automation.Pages.Contact
+{
+    class LV_ContactsActivityListPage : BaseClass
+    {
+        By btnAddActivity = By.XPath("(//button[text()='Add Activity'])[1]");
+
+        public bool VerifyUserLandsOnActivityTab()
+        {
+            return true;
+        }
+
+
+    }
+}
