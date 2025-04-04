9dfbb77de22a4d0a4c7c19adfd308f263d211dee
Sahil Mittal <sahil.mittal@technossus.com>
2025-04-04 11:42:22 +0530

    test

--- TestCases/Opportunities/TMTT0042712_VerificationOfBuyersListFeatureOnOpportunityScreen.cs
+++ TestCases/Opportunities/TMTT0042712_VerificationOfBuyersListFeatureOnOpportunityScreen.cs
@@ -269,6 +269,7 @@ namespace SF_Automation.TestCases.Opportunities
                 extentReports.CreateStepLogs("Info", "CF Financial User Logged Out from SF Lightning View. ");
 
                 driver.Quit();
+
             }
             catch (Exception e)
             {
