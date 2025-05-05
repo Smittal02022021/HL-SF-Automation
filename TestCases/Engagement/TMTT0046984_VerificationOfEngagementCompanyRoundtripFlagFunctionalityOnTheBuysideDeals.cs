94bb6c2890a25977ec173d24d49aeeb51f40a776
Sahil Mittal <Sahil.Mittal@technossus.com>
2025-05-05 10:47:21 +0530

    TMTT0046984_VerificationOfEngagementCompanyRoundtripFlagFunctionalityOnTheBuysideDeals - Initial - 5th May 2025

---
+++ TestCases/Engagement/TMTT0046984_VerificationOfEngagementCompanyRoundtripFlagFunctionalityOnTheBuysideDeals.cs
@@ -0,0 +1,240 @@
+﻿using SF_Automation.Pages.Common;
+using SF_Automation.Pages.HomePage;
+using SF_Automation.Pages;
+using SF_Automation.UtilityFunctions;
+using System;
+using NUnit.Framework;
+using SF_Automation.TestData;
+using SF_Automation.Pages.Companies;
+using SF_Automation.Pages.Engagement;
+using SF_Automation.Pages.Opportunity;
+
+namespace SF_Automation.TestCases.Engagement
+{
+    class TMTT0046984_VerificationOfEngagementCompanyRoundtripFlagFunctionalityOnTheBuysideDeals : BaseClass
+    {
+        ExtentReport extentReports = new ExtentReport();
+        LoginPage login = new LoginPage();
+        UsersLogin usersLogin = new UsersLogin();
+        HomeMainPage homePage = new HomeMainPage();
+        LVHomePage lvHomePage = new LVHomePage();
+
+        OpportunityHomePage opportunityHome = new OpportunityHomePage();
+        AddOpportunityPage addOpportunity = new AddOpportunityPage();
+        OpportunityDetailsPage opportunityDetails = new OpportunityDetailsPage();
+        EngagementDetailsPage engagementDetails = new EngagementDetailsPage();
+        LV_EngagementDetailsPage lvEngagementDetails = new LV_EngagementDetailsPage();
+        AddOpportunityContact addOpportunityContact = new AddOpportunityContact();
+
+        Outlook outlook = new Outlook();
+
+        public static string fileTMTT0046984 = "TMTT0046984_VerificationOfEngagementCompanyRoundtripFlagFunctionalityOnTheBuysideDeals";
+
+        [OneTimeSetUp]
+        public void OneTimeSetUp()
+        {
+            Initialize();
+            ExtentReportHelper();
+            ReadJSONData.Generate("Admin_Data.json");
+            extentReports.CreateTest(TestContext.CurrentContext.Test.Name);
+        }
+
+        [Test]
+        public void VerifyActivityIsLinkedToTheRelatedEngagement()
+        {
+            try
+            {
+                //Get path of Test data file
+                string excelPath = ReadJSONData.data.filePaths.testData + fileTMTT0046984;
+                string valUser = ReadExcelData.ReadData(excelPath, "Users", 1);
+                string userCAOExl = ReadExcelData.ReadData(excelPath, "Users", 2);
+
+                //Validating Title of Login Page
+                Assert.AreEqual(WebDriverWaits.TitleContains(driver, "Login | Salesforce"), true);
+                extentReports.CreateLog(driver.Title + " is displayed. ");
+
+                //Calling Login function                
+                login.LoginApplication();
+
+                //Switch to lightning view
+                if(driver.Title.Contains("Salesforce - Unlimited Edition"))
+                {
+                    homePage.SwitchToLightningView();
+                    extentReports.CreateStepLogs("Info", "User switched to lightning view. ");
+                }
+
+                //Validate user logged in
+                Assert.AreEqual(driver.Url.Contains("lightning"), true);
+                extentReports.CreateStepLogs("Passed", "Admin User is able to login into SF");
+
+                //Select HL Banker app
+                try
+                {
+                    lvHomePage.SelectAppLV("HL Banker");
+                }
+                catch(Exception)
+                {
+                    lvHomePage.SelectAppLV1("HL Banker");
+                }
+
+                //Search CF Financial user by global search
+                lvHomePage.SearchUserFromMainSearch(valUser);
+
+                //Verify searched user
+                Assert.AreEqual(WebDriverWaits.TitleContains(driver, valUser + " | Salesforce"), true);
+                extentReports.CreateLog("User " + valUser + " details are displayed ");
+
+                //Login as CF Financial user
+                lvHomePage.UserLogin();
+
+                //Switch to lightning view
+                if(driver.Title.Contains("Salesforce - Unlimited Edition"))
+                {
+                    homePage.SwitchToLightningView();
+                    extentReports.CreateStepLogs("Info", "User switched to lightning view. ");
+                }
+
+                //Validate user logged in
+                Assert.IsTrue(lvHomePage.VerifyUserIsAbleToLogin(valUser));
+                extentReports.CreateStepLogs("Passed", "CF Financial User: " + valUser + " is able to login into lightning view. ");
+
+                //Select HL Banker app
+                try
+                {
+                    lvHomePage.SelectAppLV("HL Banker");
+                }
+                catch(Exception)
+                {
+                    lvHomePage.SelectAppLV1("HL Banker");
+                }
+
+                //Navigate to Opportunities page
+                lvHomePage.NavigateToAnItemFromHLBankerDropdown("Opportunities");
+                Assert.AreEqual(WebDriverWaits.TitleContains(driver, "Recently Viewed | Opportunities | Salesforce"), true);
+                extentReports.CreateStepLogs("Passed", "User navigated to Opportunities list page. ");
+
+                string valJobType = ReadExcelData.ReadData(excelPath, "AddOpportunity", 3);
+                string valRecordType = ReadExcelData.ReadData(excelPath, "AddOpportunity", 25);
+
+                //Validating Title of New Opportunity Page
+                string pageTitle = opportunityHome.ClickNewButtonAndSelectOppRecordTypeLV(valRecordType);
+                Assert.IsTrue(pageTitle.Contains("New Opportunity"), "Verify user is on New opportunity pape for selected LOB ");
+                extentReports.CreateStepLogs("Passed", driver.Title + " is displayed ");
+
+                //Create New Opportunity
+                string opportunityName = addOpportunity.AddOpportunitiesLightningV2(valJobType, fileTMTT0046984);
+                extentReports.CreateStepLogs("Info", "Opportunity : " + opportunityName + " is created ");
+
+                //Call function to enter Internal Team details and validate Opportunity detail page
+                string displayedTab = addOpportunity.EnterStaffDetailsL(fileTMTT0046984);
+                Assert.AreEqual(displayedTab, "Info");
+                extentReports.CreateStepLogs("Info", "User is on Opportunity detail " + displayedTab + " tab ");
+
+                //Validating Opportunity details  
+                string opportunityNumber = opportunityDetails.GetOpportunityNumberL();
+                Assert.IsNotNull(opportunityNumber);
+                extentReports.CreateStepLogs("Passed", "Opportunity with number : " + opportunityNumber + " is created ");
+
+                //Create External Primary Contact
+                string valContactType = ReadExcelData.ReadData(excelPath, "AddContact", 4);
+                string valContact = ReadExcelData.ReadData(excelPath, "AddContact", 1);
+                addOpportunityContact.CickAddCFOpportunityContact();
+                addOpportunityContact.CreateContactL2(fileTMTT0046984);
+                extentReports.CreateStepLogs("Info", valContactType + " Opportunity contact is saved ");
+
+                //Update required Opportunity fields for conversion and Internal team details
+                opportunityDetails.UpdateReqFieldsForCFConversionLV2(fileTMTT0046984, valJobType);
+                extentReports.CreateStepLogs("Info", "Opportunity Required Fields for Converting into Engagement are Filled ");
+
+                opportunityDetails.UpdateInternalTeamDetailsLV(fileTMTT0046984);
+                extentReports.CreateStepLogs("Info", "Opportunity Internal Team Details are provided ");
+
+                opportunityDetails.ClickReturnToOpportunityLV();
+                extentReports.CreateStepLogs("Info", "Return to Opportunity Detail page ");
+
+                //update CC and NBC checkboxes in LV
+                opportunityDetails.UpdateOutcomeNBCApproveDetailsLV(valJobType);
+                extentReports.CreateStepLogs("Info", "CC and NBC checkboxes updated. ");
+
+                //Requesting for engagement and validate the success message
+                opportunityDetails.ClickRequestToEngL();
+
+                //Submit Request To Engagement Conversion 
+                string msgSuccess = opportunityDetails.GetRequestToEngMsgL();
+                Assert.AreEqual(msgSuccess, "Opportunity has been submitted for Approval.");
+                extentReports.CreateStepLogs("Passed", "Success message: " + msgSuccess + " is displayed ");
+
+                //TC - End
+                lvHomePage.UserLogoutFromSFLightningView();
+                extentReports.CreateStepLogs("Info", "CF Financial User Logged Out from SF Lightning View. ");
+
+                //Select HL Banker app
+                try
+                {
+                    lvHomePage.SelectAppLV("HL Banker");
+                }
+                catch(Exception)
+                {
+                    lvHomePage.SelectAppLV1("HL Banker");
+                }
+
+                //Search CAO Financial user by global search
+                lvHomePage.SearchUserFromMainSearch(userCAOExl);
+
+                //Verify searched user
+                Assert.AreEqual(WebDriverWaits.TitleContains(driver, userCAOExl + " | Salesforce"), true);
+                extentReports.CreateLog("User " + userCAOExl + " details are displayed ");
+
+                //Login as CAO user
+                lvHomePage.UserLogin();
+
+                //Switch to lightning view
+                if(driver.Title.Contains("Salesforce - Unlimited Edition"))
+                {
+                    homePage.SwitchToLightningView();
+                    extentReports.CreateStepLogs("Passed", "CAO User: " + userCAOExl + " is able to login into lightning view. ");
+                }
+                else
+                {
+                    extentReports.CreateStepLogs("Passed", "CAO User: " + userCAOExl + " is able to login into lightning view. ");
+                }
+
+                Assert.IsTrue(lvHomePage.VerifyUserIsAbleToLogin(userCAOExl));
+                extentReports.CreateStepLogs("Passed", "CAO User: " + userCAOExl + " is able to login into lightning view. ");
+
+                //Search for created opportunity
+                extentReports.CreateStepLogs("Info", " CAO User Search for Created Opportunity");
+                opportunityHome.SearchOpportunitiesInLightningView(opportunityName);
+
+                //Approve the Opportunity 
+                string status = opportunityDetails.ClickApproveButtonLV2();
+                Assert.AreEqual(status, "Approved");
+                extentReports.CreateStepLogs("Passed", "Opportunity " + status + " ");
+                opportunityDetails.CloseApprovalHistoryTabL();
+
+                //Calling function to convert to Engagement
+                opportunityDetails.ClickConvertToEngagementL2();
+                extentReports.CreateStepLogs("Info", "Opportunity Converted into Engagement ");
+
+                //Validate the Engagement name in Engagement details page
+                string engagementNumber = engagementDetails.GetEngagementNumberL();
+                string engagementName = engagementDetails.GetEngagementNameL();
+
+                //Need to get Name of Opp and Eng
+                Assert.AreEqual(opportunityName, engagementName);
+                extentReports.CreateStepLogs("Passed", "Name of Engagement : " + engagementName + " is Same as Opportunity name ");
+
+                //TC - End
+                lvHomePage.UserLogoutFromSFLightningView();
+                extentReports.CreateStepLogs("Info", "CF Financial User Logged Out from SF Lightning View. ");
+
+                driver.Quit();
+            }
+            catch (Exception e)
+            {
+                extentReports.CreateExceptionLog(e.Message);
+                usersLogin.UserLogOut();
+            }
+        }
+    }
+}
